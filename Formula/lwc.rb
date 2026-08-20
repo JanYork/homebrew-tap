5c1096b5f0d146252f0c09f2b870df5d1afd0e80
JanYork <ixor@qq.com>
2026-08-20 20:34:28 +0800

    lwc 0.15.2: drop stale bottles

--- Formula/lwc.rb
+++ Formula/lwc.rb
@@ -6,12 +6,6 @@ class Lwc < Formula
   license "Apache-2.0"
   head "https://github.com/JanYork/llm-wiki-cli.git", branch: "main"
 
-  bottle do
-    root_url "https://github.com/JanYork/homebrew-tap/releases/download/lwc-0.14.6"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "540194e4b34d50e1276be367bb4ff25f4fffd59c22ef71057d27c03b34f9a090"
-    sha256 cellar: :any,                 x86_64_linux: "47b5edfa782cb16a0cdf14a55c0dd4d25f9fb28748072f7b95f2fa1332f1e0e3"
-  end
-
   depends_on "rust" => :build
 
   def install
